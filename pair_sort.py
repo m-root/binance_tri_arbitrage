8b6087949433eb2a83a7a83c5890b9db4968afa8
Muema Kamba <muemakamba@gmail.com>
2022-07-02 06:58:40 +0300

    Finished alpha forward and reverse pair sort

---
+++ pair_sort.py
@@ -0,0 +1,27 @@
+# todo
+# Finished alpha forward and reverse pair sort
+def separator(ab, b):
+    if b.index(ab) == 0:
+        return b[1]
+    else:
+        return b[0]
+import time
+
+def pair_sorting(start_item, array_edit):
+    new_arr = []
+    time_start = time.time()
+    print(time_start)
+    while len(array_edit) > 0:
+        for aa in array_edit:
+            if start_item not in aa:
+                aa = [b for b in array_edit if start_item in b][0]
+                print(aa)
+            ans = separator(start_item, aa)
+            print(ans)
+            array_edit.remove(aa)
+            start_item = ans
+            new_arr.append(aa)
+    print(time.time()-time_start)
+    print(new_arr)
+    return new_arr
+
