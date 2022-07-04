a68e930a9f4bbdc6a0e47e2c9a4a28eff6dc8540
Muema Kamba <muemakamba@gmail.com>
2022-07-04 12:09:26 +0300

    Orderbook depth calculation and test

--- payload/get_orderbook_depth.py
+++ payload/get_orderbook_depth.py
@@ -1,53 +1,3 @@
-# arr = [[6, 1], [1, 2], [3, 4], [5, 6], [2, 3], [4, 5]]
-# '''
-# start_item = 1 or a and pair is b
-# array with start item = last_item
-# pair_part of start_item = b
-# '''
-#
-# def separator(ab, b):
-#     if b.index(ab) == 0:
-#         return b[1]
-#     else:
-#         return b[0]
-# import time
-#
-# def pair_sorting(start_item = 1, array_edit = arr):
-#     new_arr = []
-#     time_start = time.time()
-#     print(time_start)
-#     while len(array_edit) > 0:
-#         for aa in array_edit:
-#             if start_item not in aa:
-#                 aa = [b for b in array_edit if start_item in b][0]
-#                 print(aa)
-#             ans = separator(start_item, aa)
-#             print(ans)
-#             array_edit.remove(aa)
-#             start_item = ans
-#             new_arr.append(aa)
-#     print(time.time()-time_start)
-#     print(new_arr)
-#     return new_arr
-#
-# pair_sorting()
-import time
-
-import binance
-
-# for d in binance.Client().get_order_book(symbol='BTCUSDT')['asks']:
-#     print(d)
-
-
-# print(c)
-
-'''
-num = 10
-while (num >= 0):
-    num -= 1
-    print(num)
-    
-'''
 import time
 
 a = [['19115.22000000', '3.99830000'], ['19115.23000000', '0.00073000'], ['19115.27000000', '0.56735000'],
@@ -132,7 +82,3 @@ def order_book_orders(orders=a, qty=5):
 
 
 print(order_book_orders())
-
-'''
-76428.384126+13.954117899999998+10845.0484345+419.773086+5407.9321558+1212.3126204
-'''
